105a54d2eb6cef1add2dacf4a162807de1e7b8cc
Divyanshdubey001 <124764197+Divyanshdubey001@users.noreply.github.com>
2023-03-17 17:26:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,51 +1,30 @@
-const http = require("http");
-const fs = require("fs");
-const port = 3000;
+const express = require("express");
+const server = express();
+const path = require("path");
+const port = 8081;
 
-http
-  .createServer((req, res) => {
-    console.log(req.url);
-    if ((req.url = "/")) {
-      fs.readFile("about.html", "utf-8", (err, data) => {
-        if (err) {
-          console.error(err);
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          res.end();
-          console.log(data);
-        }
-      });
-    } else if ((req.url = "/index")) {
-      fs.readFile("index.html", "utf-8", (err, data) => {
-        console.log(req.url);
-        if (err) {
-          console.error(err);
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          res.end();
-          console.log(data);
-        }
-      });
-    } else if (req.url == "/contact") {
-      fs.readFile("contact.html", "utf-8", (err, data) => {
-        console.log(req.url);
-        if (err) {
-          console.error(err);
-        } else {
-          res.writeHead(200, { "Content-Type": "text/html" });
-          res.write(data);
-          res.end();
-          console.log(data);
-        }
-      });
-    }
-  })
-  .listen(port, function (error) {
-    if (error) {
-      console.log("Something went wrong", error);
-    } else {
-      console.log("Server running as port " + port);
-    }
-  });
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+server.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "about.html"));
+});
+
+server.get("/contact", (req, res) => {
+  res.sendFile(path.join(__dirname, "contact.html"));
+});
+
+//css file
+server.get("/css", (req, res) => {
+  res.sendFile(path.join(__dirname, "stylesheet" , "styles.css"));
+});
+
+// Start the server
+server.listen(port, function (error) {
+  if (error) {
+    console.error("Something went wrong", error);
+  } else {
+    console.log("Server running as port " + port);
+  }
+});

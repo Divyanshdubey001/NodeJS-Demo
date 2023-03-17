e72435733c5ba7d1ce81608f0a8dc3ba1b7a24e0
Divyanshdubey001 <124764197+Divyanshdubey001@users.noreply.github.com>
2023-03-17 18:19:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,65 +1,6 @@
-const express = require("express");
-const server = express();
-const path = require("path");
-const port = 8081;
-
-server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-// server.get("/", (req, res) => {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
-
-server.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "about.html"));
-});
-
-server.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname, "contact.html"));
-});
-
-//css file
-server.get("/css", (req, res) => {
-  res.sendFile(path.join(__dirname, "stylesheet" , "styles.css"));
-});
-
-// Start the server
-server.listen(port, function (error) {
-  if (error) {
-    console.error("Something went wrong", error);
-  } else {
-    console.log("Server running as port " + port);
-  }
-});
-
-// server.get("/about", (req, res) => {
-//   res.sendFile(path.join(__dirname, "about.html"));
-// });
-
-// server.get("/contact", (req, res) => {
-//   res.sendFile(path.join(__dirname, "contact.html"));
-// });
-
-// //css file
-// server.get("/css", (req, res) => {
-//   res.sendFile(path.join(__dirname, "stylesheet" , "styles.css"));
-// });
-
-// // Start the server
-// server.listen(port, function (error) {
-//   if (error) {
-//     console.error("Something went wrong", error);
-//   } else {
-//     console.log("Server running as port " + port);
-//   }
-// });
-
-     console.log("Server running as port dgfdgd ");
-
-
 const http = require("http");
 const fs = require("fs");
-// const port = 3000;
+const port = 3000;
 http
   .createServer((req, res) => {
     console.log(req.url);

f2d49116849c13560ee76638938f0a39e22d8f87
Divyanshdubey001 <divyanshdubey@technogetic.com>
2023-03-17 17:56:52 +0530

    final push

--- index.js
+++ index.js
@@ -1,30 +1,102 @@
-const express = require("express");
-const server = express();
-const path = require("path");
-const port = 8081;
+// const express = require("express");
+// const server = express();
+// const path = require("path");
+// const port = 8081;
 
-server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+// server.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
 
-server.get("/about", (req, res) => {
-  res.sendFile(path.join(__dirname, "about.html"));
-});
+// server.get("/about", (req, res) => {
+//   res.sendFile(path.join(__dirname, "about.html"));
+// });
 
-server.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname, "contact.html"));
-});
+// server.get("/contact", (req, res) => {
+//   res.sendFile(path.join(__dirname, "contact.html"));
+// });
 
-//css file
-server.get("/css", (req, res) => {
-  res.sendFile(path.join(__dirname, "stylesheet" , "styles.css"));
-});
+// //css file
+// server.get("/css", (req, res) => {
+//   res.sendFile(path.join(__dirname, "stylesheet" , "styles.css"));
+// });
 
-// Start the server
-server.listen(port, function (error) {
-  if (error) {
-    console.error("Something went wrong", error);
-  } else {
-    console.log("Server running as port " + port);
-  }
-});
+// // Start the server
+// server.listen(port, function (error) {
+//   if (error) {
+//     console.error("Something went wrong", error);
+//   } else {
+//     console.log("Server running as port " + port);
+//   }
+// });
+
+const http = require("http");
+const fs = require("fs");
+const port = 3000;
+http
+  .createServer((req, res) => {
+    console.log(req.url);
+    if ((req.url = "/")) {
+      fs.readFile("about.html", "utf-8", (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+          console.log(data);
+        }
+      });
+    } else if ((req.url = "/index")) {
+      fs.readFile("index.html", "utf-8", (err, data) => {
+        console.log(req.url);
+        if (err) {
+          console.error(err);
+          console.log(data);
+        }
+      });
+    } else if (req.url == "/contact") {
+      fs.readFile("contact.html", "utf-8", (err, data) => {
+        console.log(req.url);
+        if (err) {
+          console.error(err);
+        } else {
+          res.writeHead(200, { "Content-Type": "text/html" });
+          res.write(data);
+          res.end();
+          console.log(data);
+        }
+      });
+    } else if (req.url == "/css") {
+      fs.readFile("styles.css", "utf-8", (err, data) => {
+        console.log(req.url);
+        if (err) {
+          console.error(err);
+        } else {
+          res.writeHead(200, { "Content-Type": "styles.css" });
+          res.write(data);
+          res.end();
+          console.log(data);
+        }
+      });
+    }
+    else if (req.url == "/img") {
+      fs.readFile("img1.jpeg", (err, data) => {
+        console.log(req.url);
+        if (err) {
+          console.error(err);
+        } else {
+          res.writeHead(200, { "Content-Type": "img.jpeg" });
+          res.write(data);
+          res.end();
+          console.log(data);
+        }
+      });
+    }
+  })
+  .listen(port, function (error) {
+    if (error) {
+      console.log("Something went wrong", error);
+    } else {
+      console.log("Server running as port " + port);
+    }
+  });
